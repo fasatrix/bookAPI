106bb13e96d95868c416575e1a0ac6ee7d33ebcc
fasatrix <fasatrix@gmail.com>
2017-08-27 07:22:47 +1200

    changes process.env

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 var express = require('express'),
 	mongoose = require('mongoose'),
 	bodyParser = require('body-parser');
-var db = mongoose.connect('mongodb://appuser:L0P1Tst#1@ds149763.mlab.com:49763/bookapi');
+var db = mongoose.connect('mongodb://process.env.DB_USERNAME:process.env.DB_PASSWORD@ds149763.mlab.com:49763/bookapi');
 // var db = mongoose.connect('mongodb://localhost/bookAPI');
 var Book = require('./models/bookModel');
 
 var app = express();
 var port  = process.env.PORT || 3000;
-
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json())
